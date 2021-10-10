eb68d000f5d00a8b46cfcba5d30eb46d04700e69
Andrii-OttO <82340217+Andrii-OttO@users.noreply.github.com>
2021-10-10 09:13:18 +0300

    исправил переключение ползунка

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import data from './menu.json'
 import template from './menu.handlebars'
-//console.log(template)
+
+document.querySelector('body').classList.add('light-theme')
 let menuCards = template(data);
 const checkBoxs = document.getElementById('theme-switch-toggle');
 const menuList = document.querySelector('.js-menu');
@@ -10,12 +11,16 @@ checkBoxs.addEventListener('change', (e)=>{
 if(e.target.checked){
     document.querySelector('body').classList.add('dark-theme')
     localStorage.setItem('theme', 'dark-theme')
+    document.querySelector('body').classList.remove('light-theme')
 }
 else{
     document.querySelector('body').classList.remove('dark-theme')
-    localStorage.setItem('theme','')}
+    localStorage.setItem('theme','light-theme')
+    document.querySelector('body').classList.add('light-theme')
+}
 })
+
 let theme = localStorage.getItem('theme')
 if(theme === 'dark-theme'){
-    document.querySelector('body').classList.add('dark-theme')
-}else{document.querySelector('body').classList.remove('dark-theme')}
+    checkBoxs.checked = true
+}
